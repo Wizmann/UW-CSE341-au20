60f41cc92e41a61dd382df38a10b16d7716e7802
Wizmann <mail.kuuy@gmail.com>
2020-12-01 21:44:26 +0800

    first 8 problem of hw2

--- hw2/hw2.ml
+++ hw2/hw2.ml
@@ -45,9 +45,37 @@ let sort ls = List.sort compare ls
 #print_depth 10;;
 #print_length 1000;;
 
+let rec range (i: int) (j: int) : int list =
+    if i > j then
+        []
+    else
+        i :: (range (i + 1) j)
+;;
+
+let rec reverse items : 'a list =
+    match items with
+    | [] -> []
+    | hd :: tl -> reverse(tl) @ [ hd ]
+;;
 
 (* Part 0: Warm-up *)
-(**** PUT PROBLEM 1 HERE ****)
+
+(* Problem 1
+ * Write a function make_silly_json that takes an int i and returns a json.
+ * The result should represent a JSON array of JSON objects where every object
+ * in the array has two fields, "n" and "b". Every object's "b" field should hold
+ * true (i.e., True). The first object in the array should have a "n" field holding
+ * the JSON number i.0 (in other words, the integer i converted to a floating point
+ * JSON number), the next object should have an "n" field holding (i - 1).0 and so on
+ * where the last object in the array has an "n" field holding 1.0.
+ * Sample solution is less than 10 lines. Hints: There's a function in OCaml's
+ * standard library called float_of_int that converts an integer to a float.
+ * You'll want a helper function that does most of the work.
+*)
+
+let make_silly_json(n: int) : json = 
+    Array (range 1 n |> reverse |> List.map (fun i -> Object [("n", Num (float_of_int i)); ("b", True)]))
+;;
 
 (* Part 1: Printing JSON values *)
 
@@ -59,7 +87,55 @@ let json_string_of_float (f : float) : string =
 
 (**** PUT PROBLEMS 2-4 HERE ****)
 
+(* Problem 2
+ * Write a function concat_with that takes a separator string and a list of strings,
+ * and returns the string that consists of all the strings in the list concatenated
+ * together, separated by the separator. The separator should be only between elements,
+ * not at the beginning or end. Use OCaml's ^ operator for concatenation (e.g.,
+ * "hello" ^ "world" evaluates to "helloworld"). Sample solution is 5 lines.
+*)
 
+let rec concat_with (delimeter: string) (strs: string list) : string =
+    match strs with
+    | [] -> ""
+    | hd :: [] -> hd
+    | hd :: tl -> hd ^ delimeter ^ (concat_with delimeter tl)
+;;
+
+(* Problem 3
+ * Write a function quote_string that takes a string and returns a string that is the
+ * same except there is an additional '"' character (a single quote character) at the
+ * beginning and end.
+ * Sample solution is 1 line.
+*)
+
+let quote_string(str: string) : string =
+    "\"" ^ str ^ "\""
+;;
+
+(* Problem 4
+ * Write a function string_of_json that converts a json into the proper string encoding in
+ * terms of the syntax described on the first page of this assignment. The two previous
+ * problems are both helpful, but you will also want local helper functions for processing
+ * arrays and objects (hint: in both cases, create a string list that you then pass to concat_with).
+ * In the Num case, use the provided json_string_of_float function.
+ * Sample solution is 25 lines.
+*)
+
+let rec string_of_json(obj: json) : string =
+    let rec make_key_value (kv: string * json) : string =
+        match kv with
+        | (key, value) -> (quote_string key) ^ " : " ^ (string_of_json value)
+    in
+
+    match obj with
+    | Num (num) -> (json_string_of_float num)
+    | String (str) -> (quote_string str)
+    | False -> "false"
+    | True -> "true"
+    | Null -> "null"
+    | Array (sub_objs) -> "[" ^ (concat_with ", " (List.map string_of_json sub_objs)) ^ "]"
+    | Object (sub_objs) -> "{" ^ (concat_with ", " (List.map make_key_value sub_objs)) ^ "}"
 
 (* Part 2: Processing JSON values *)
 
@@ -71,6 +147,73 @@ type point = { latitude: float; longitude: float }
 (**** PUT PROBLEMS 5-20 HERE ****)
 
 (* Don't forget to write a comment for problems 7 and 20. *) 
+
+(* Problem 5
+ * Write a function take of type int * 'a list -> 'a list that takes an int called n and
+ * a list called l and returns the first n elements of l in the same order as l. You may
+ * assume that n is non-negative and does not exceed the length of l.
+ * Sample solution is about 5 lines.
+*)
+
+let rec take (n: int) (l: 'a list) : 'a list =
+    match l with
+    | [] -> ignore(assert(n = 0)); []
+    | hd :: tl -> 
+            if n = 0 then [] else hd :: (take (n - 1)  tl)
+;;
+
+(* Problem 6
+ * Write a function firsts of type ('a * 'b) list -> 'a list that takes a list of pairs
+ * and returns a list of all the first components of the pairs in the same order as the
+ * argument list.
+ * Sample solution is about 4 lines.
+*)
+let rec firsts(pairs : ('a * 'b) list) : 'a list =
+    match pairs with
+    | [] -> []
+    | (item1, item2) :: tl -> item1 :: firsts(tl)
+
+(* Problem 7
+ * Write a comment in your file after your definition of firsts answering the following
+ * questions. Suppose l has type (int * int) list, and let n be an integer between 0 and
+ * the length of l (inclusive), and consider the expressions firsts (take (n, l)) and
+ * take (n, firsts l). Either (1) write one sentence explaining in informal but precise
+ * English why these two expressions always evaluate to the same value; or (2) give example
+ * values of l and n such that the two expressions evaluate to different values.
+ * Regardless of whether you decide option (1) or option (2) is correct, also write one
+ * sentence explaining which of the two expressions above might be faster to evaluate and why.
+*)
+
+(* Answer for Problem 7:
+ *
+ * Assuming we have a list `l` which `length(l) >= n + 1`,
+ * and a function `nth` to retrieve the nth element of the list.
+
+ * It's easy for us to know that:
+ * ```
+ * f(n) = (firsts (take n l)) = (firsts (take n l)) @ (firsts (nth (n + 1) l))
+ * g(n) = (take n (firsts l)) = (take n (firsts l)) @ (firsts (nth (n + 1) l))
+ * ```
+
+ * It means for a specific length `n`, if f(n) = g(n), we can prove f(n + 1) = g(n + 1).
+ * And it's trival that f(0) = g(0). We can use mathmatical induction to prove that
+ * for every n, there will be f(n) = g(n)
+*)
+
+(* Problem 8
+ * Write a function assoc of type 'a * ('a * 'b) list -> 'b option that takes two arguments
+ * k and xs. It should return Some v1 if (k1,v1) is the pair in the list closest to the beginning
+ * of the list for which k and k1 are equal. If there is no such pair, assoc returns None.
+ * Sample solution is a few lines. (Note: There's a function with similar functionality in the
+ * OCaml standard library, but calling it requires features we haven't covered yet.
+ * Do not use that function or you will not receive credit.)
+*)
+
+let rec assoc (key : 'a) (xs : ('a * 'b) list) : 'b option =
+    match xs with
+    | [] -> None
+    | (k, v) :: tl -> if key = k then Some v else (assoc key tl)
+;;
 
 
 (* histogram and histogram_for_access_path are provided, but they use your
--- hw2/hw2test.ml
+++ hw2/hw2test.ml
@@ -4,26 +4,67 @@
 
 (* You will surely want to add more! *)
 
-let test1 = 
-  make_silly_json 2 
-  = 
-  Array
-    [Object [("n", Num 2.); ("b", True)]; 
-     Object [("n", Num 1.); ("b", True)]]
+(* Problem 1 *)
+let () = 
+  assert( (make_silly_json 2) = 
+      Array [Object [("n", Num 2.); ("b", True)]; Object [("n", Num 1.); ("b", True)]]
+  )
 
-let test2 = concat_with (";", ["1"; "2"]) = "1;2"
+let () = 
+  assert( (make_silly_json 1) = 
+      Array [Object [("n", Num 1.); ("b", True)]; ]
+  )
 
-let test3 = quote_string "hello" = "\"hello\""
+let () = 
+  assert( (make_silly_json 0) = Array [] )
 
-let test4 = string_of_json json_obj = "{\"foo\" : 3.14159, \"bar\" : [1, \"world\", null], \"ok\" : true}"
+(* Problem 2 *)
+let () = assert( (concat_with ";" ["1"; "2"]) = "1;2" )
+let () = assert( (concat_with ";" ["xyz"]) = "xyz" )
+let () = assert( (concat_with ";" []) = "" )
 
-let test5 = take (2, [4; 5; 6; 7]) = [4; 5]
+(* Problem 3 *)
+let () = assert( (quote_string "hello") = "\"hello\"")
+let () = assert( (quote_string "") = "\"\"")
 
-let test6 = firsts [(1,2); (3,4)] = [1; 3]
+(* Problem 4 *)
+let () = assert( (string_of_json json_pi) = "3.14159" )
+let () = assert( (string_of_json json_hello) = "\"hello\"" )
+let () = assert( (string_of_json json_false) = "false" )
+let () = assert( (string_of_json json_array) = "[1, \"world\", null]" )
+let () = assert(
+    (string_of_json json_obj) = "{\"foo\" : 3.14159, \"bar\" : [1, \"world\", null], \"ok\" : true}")
 
-let test7 = String.length "don't forget to write a comment!" > 0
+let obj1 = Object [("foo", json_pi)]
+let () = assert( (string_of_json obj1) = "{\"foo\" : 3.14159}" )
 
-let test8 = assoc ("foo", [("bar",17);("foo",19)]) = Some 19
+let obj2 = Object [("foo", json_hello); ("bar", Object [("baz", String "bazbaz"); ("array", json_array)])]
+let () = assert(
+    (string_of_json obj2) = "{\"foo\" : \"hello\", \"bar\" : {\"baz\" : \"bazbaz\", \"array\" : [1, \"world\", null]}}")
+
+(* Problem 5 *)
+let () = assert( (take 0 [4; 5; 6; 7]) = [] )
+let () = assert( (take 1 [4; 5; 6; 7]) = [4] )
+let () = assert( (take 2 [4; 5; 6; 7]) = [4; 5] )
+let () = assert( (take 3 [4; 5; 6; 7]) = [4; 5; 6] )
+let () = assert( (take 4 [4; 5; 6; 7]) = [4; 5; 6; 7] )
+let () = assert( (take 2 ["No.1"; "No.2"; "No.3"; "No.4"]) = ["No.1"; "No.2"] )
+
+(* Problem 6 *)
+let () = assert( (firsts [(1,2); (3,4)]) = [1; 3])
+
+(* Problem 7 *)
+let () =
+    let l = [(1, 2); (3, 4); (5, 6)] in
+    assert( (firsts (take 2 l)) = (take 2 (firsts l)))
+
+(* Problem 8 *)
+
+let () = assert( (assoc "foo" [("bar", 17);("foo", 19)]) = Some 19)
+let () = assert( (assoc 12 [(12, 17);(13, 19)]) = Some 17)
+let () = assert( (assoc 999 [(12, 17);(13, 19)]) = None)
+
+(*
 
 let test9 = dot (json_obj, "ok") = Some True
 
@@ -96,3 +137,6 @@ let testC6 = expect (Colon, [Colon; FalseTok]) = [FalseTok]
 (* Test for parse_json. You'll want more. *)
 let testC10 = parse_json (tokenize "{ \"foo\" : null, \"bar\" : [true, false] }")
               = (Object [("foo", Null); ("bar", Array [True; False])], [])
+*)
+
+let () = print_endline("HW2 test OK")

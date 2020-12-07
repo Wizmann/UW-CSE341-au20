3c8e6c3c289ded79bdbefafbd2b08be76db572c9
Wizmann <mail.kuuy@gmail.com>
2020-12-08 00:35:47 +0800

    refine code

--- hw2/hw2test.ml
+++ hw2/hw2test.ml
@@ -247,32 +247,4 @@ let () = assert(
 (* Problem 25 *)
 let () = assert( (List.length all_fourty_fours) = 6 )
 
-(* ---------Challenge Problems------------- *)
-
-(* Problem C1 *)
-
-let () = assert(
-    consume_string_literal (char_list_of_string "\"foo\" : true") = ("foo", [' '; ':'; ' '; 't'; 'r'; 'u'; 'e']))
-
-(*
-
-(* Commented out tests for challenge problems *)
-
-
-let testC2 = consume_keyword (char_list_of_string "false foo") = (FalseTok, [' '; 'f'; 'o'; 'o'])
-
-(* Test for tokenize_char_list. You'll want more. *)
-let testC3 = tokenize_char_list (char_list_of_string "{ \"foo\" : 3.14, \"bar\" : [true, false] }")
-             = [LBrace; StringLit "foo"; Colon; NumLit "3.14"; Comma; StringLit "bar";
-                Colon; LBracket; TrueTok; Comma; FalseTok; RBracket; RBrace]
-
-let testC5 = parse_string ([StringLit "foo"; FalseTok]) = ("foo", [FalseTok])
-
-let testC6 = expect (Colon, [Colon; FalseTok]) = [FalseTok]
-
-(* Test for parse_json. You'll want more. *)
-let testC10 = parse_json (tokenize "{ \"foo\" : null, \"bar\" : [true, false] }")
-              = (Object [("foo", Null); ("bar", Array [True; False])], [])
-*)
-
 let () = print_endline("HW2 test OK")

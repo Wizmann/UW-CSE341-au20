73d61acb6f6b16f0edbf6c58a13aa796d5a2ff42
Wizmann <mail.kuuy@gmail.com>
2020-12-20 15:21:03 +0800

    init

---
+++ hw6/hw6tests.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(require "hw6.rkt")
+
+; This file uses Racket's unit-testing framework, which is convenient but not required of you.
+
+; Note we have provided [only] 3 tests, but you can't run them until do some of the assignment.
+; You will want more tests.
+
+(require rackunit)
+
+(define tests
+  (test-suite
+   "Homework 6 Tests"
+
+   (check-equal? (eval-exp (add (int 2) (int 2))) (int 4) "add simple test")
+
+   (check-exn (lambda (x) (string=? (exn-message x) "MUPL addition applied to non-number"))
+              (lambda () (eval-exp (add (int 2) (munit))))
+              "add bad argument")
+
+   (check-equal? (mupllist->racketlist
+                  (eval-exp (call (call mupl-all-gt (int 9))
+                                  (racketlist->mupllist 
+                                   (list (int 10) (int 9) (int 15))))))
+                 (list (int 10) (int 15))
+                 "provided combined test using problems 2, 3, and 5")
+   ))
+
+(require rackunit/text-ui)
+;; runs the test
+(run-tests tests)

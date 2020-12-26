565912669cc105ed3352c43cdacc5ea56786ceb4
Wizmann <mail.kuuy@gmail.com>
2020-12-26 17:32:50 +0800

    finish problem 2/3

--- hw6/hw6.rkt
+++ hw6/hw6.rkt
@@ -23,9 +23,31 @@
 
 ;; Problem 2
 
-;; CHANGE (put your solutions here)
+;; (a) Write a Racket function racketlist->mupllist that takes a Racket list (presumably of mupl
+;; values but that will not affect your solution) and produces an analogous mupl list with the same
+;; elements in the same order.
+(define (racketlist->mupllist items)
+  (if (null? items)
+      munit
+      (apair (car items) (racketlist->mupllist (cdr items)))))
+
+;; (b) Write a Racket function mupllist->racketlist that takes a mupl list (presumably of mupl
+;; values but that will not affect your solution) and produces an analogous Racket list (of mupl
+;; values) with the same elements in the same order.
+(define (mupllist->racketlist items)
+  (if (apair? items)
+      (cons (apair-e1 items) (mupllist->racketlist (apair-e2 items)))
+      null))
 
 ;; Problem 3
+
+;; Implementing the mupl Language: Write a mupl interpreter, i.e., a Racket function eval-exp
+;; that takes a mupl expression e and either returns the mupl value that e evaluates to under the empty
+;; environment or calls Racket’s error if evaluation encounters a run-time mupl type error or unbound
+;; mupl variable.
+;; A mupl expression is evaluated under an environment (for evaluating variables, as usual). In your
+;; interpreter, use a Racket list of Racket pairs to represent this environment (which is initially empty)
+;; so that you can use without modification the provided envlookup function.
 
 ;; lookup a variable in an environment
 ;; Do NOT change this function
@@ -34,13 +56,28 @@
         [(equal? (car (car env)) str) (cdr (car env))]
         [#t (envlookup (cdr env) str)]))
 
+(define (extract-int-value num)
+  (if (int? num) (int-num num)
+    (error "extract int value from non-number of MUPL")))
+
 ;; Do NOT change the two cases given to you.  
 ;; DO add more cases for other kinds of MUPL expressions.
 ;; We will test eval-under-env by calling it directly even though
 ;; "in real life" it would be a helper function of eval-exp.
 (define (eval-under-env e env)
-  (cond [(var? e) 
+  (begin
+  (displayln e)
+  (cond
+        ;; All values (including closures) evaluate to themselves. For example, (eval-exp (int 17)) would
+        ;; return (int 17), not 17.
+        [(int? e) e]
+        [(munit? e) e]
+        [(closure? e) e]
+        ;; A variable evaluates to the value associated with it in the environment.
+        [(var? e) 
          (envlookup env (var-string e))]
+        ;; An addition evaluates its subexpressions and, assuming they both produce integers, produces the
+        ;; integer that is their sum. (Note this case is done for you to get you pointed in the right direction.)
         [(add? e) 
          (let ([v1 (eval-under-env (add-e1 e) env)]
                [v2 (eval-under-env (add-e2 e) env)])
@@ -49,12 +86,90 @@
                (int (+ (int-num v1) 
                        (int-num v2)))
                (error "MUPL addition applied to non-number")))]
-        ;; CHANGE add more cases here
-        [#t (error (format "bad MUPL expression: ~v" e))]))
+        ;; An isgreater evaluates its two subexpressions to values v 1 and v 2 respectively. If both values
+        ;; are integers, then if v 1 > v 2 the result of the isgreater expression is the mupl value (int 1), else
+        ;; the result is the mupl value (int 0).
+        [(isgreater? e)
+         (let ([v1 (eval-under-env (isgreater-e1 e) env)]
+               [v2 (eval-under-env (isgreater-e2 e) env)])
+          (begin
+          (if (and (int? v1) (int? v2))
+              (if (> (extract-int-value v1) (extract-int-value v2)) (int 1) (int 0))
+              (error "MUPL isgreater applied to non-number"))))]
+        ;; An ifnz evaluates its first expression to a value v 1 . If it is an integer, then if it is not zero, then
+        ;; ifnz evaluates its second subexpression, else it evaluates its third subexpression.
+        [(ifnz? e)
+         (let ([v1 (eval-under-env (ifnz-e1 e) env)]
+               [e2 (eval-under-env (ifnz-e2 e) env)]
+               [e3 (eval-under-env (ifnz-e3 e) env)])
+          (if (= (extract-int-value v1) 0) e2 e3))]
+
+        ;; An mlet expression evaluates its first expression to a value v. Then it evaluates the second
+        ;; expression to a value, in an environment extended to map the name in the mlet expression to v.
+        [(mlet? e)
+          (let ([var (mlet-var e)]
+                [exp (eval-under-env (mlet-e e) env)]
+                [body (mlet-body e)])
+          (eval-under-env body (cons (cons var (eval-under-env exp env)) env)))]
+
+        ;; apair expression evaluates its two subexpressions and produces a (new) pair holding the results.
+        [(apair? e)
+         (let ([e1 (eval-under-env (apair-e1 e) env)]
+               [e2 (eval-under-env (apair-e2 e) env)])
+          (apair e1 e2))]
+
+        ;; A first expression evaluates its subexpression. If the result for the subexpression is a pair, then
+        ;; the result for the first expression is the e1 field in the pair.
+        [(first? e)
+          (let ([pair (first-e e)])
+           (if (apair? pair)
+            (apair-e1 pair)
+            (error (format "MUPL first applied to non-pair"))))]
+
+        ;; A second expression evaluates its subexpression. If the result for the subexpression is a pair, then
+        ;; the result for the second expression is the e2 field in the pair.
+        [(second? e)
+          (let ([pair (second-e e)])
+           (if (apair? pair)
+            (apair-e2 pair)
+            (error (format "MUPL second applied to non-pair"))))]
+
+        ;; * An ismunit expression evaluates its subexpression. If the result is an munit expression, then the
+        ;; result for the ismunit expression is the mupl value (int 1), else the result is the mupl value (int 0).
+        [(ismunit? e)
+          (let ([v (eval-under-env e env)])
+            (if (munit? v) (int 1) (int 0)))]
+
+        ;; Functions are lexically scoped: A function evaluates to a closure holding the function and the
+        ;; current environment.
+        [(fun? e)
+          (let ([funname (fun-nameopt e)])
+           (closure
+            (if (null? funname) env (cons (cons funname e) env))
+            e))]
+
+        ;; A call evaluates its first and second subexpressions to values. If the first is not a closure, it is
+        ;; an error. Else, it evaluates the closure’s function’s body in the closure’s environment extended
+        ;; to map the function’s name to the closure (unless the name field is null) and the function’s
+        ;; argument-name (i.e., the parameter name) to the result of the second subexpression.
+        [(call? e)
+          (let ([funexp (call-funexp e)]
+                [actual (call-actual e)])
+           (if (closure? funexp)
+            (let* ([fun (closure-fun funexp)]
+                   [formal (fun-formal fun)]
+                   [funenv (closure-env funexp)])
+             (eval-under-env (fun-body fun) (cons (cons formal actual) e)))
+            (error "MUPL can't call a function that isn't a closure")))]
+
+        [#t (error (format "bad MUPL expression: ~v" e))])))
 
 ;; Do NOT change
 (define (eval-exp e)
   (eval-under-env e null))
+
+;(displayln (eval-exp (first (apair (int 1) (int 2)))))
+
         
 ;; Problem 4
 
--- hw6/hw6tests.rkt
+++ hw6/hw6tests.rkt
@@ -13,19 +13,54 @@
   (test-suite
    "Homework 6 Tests"
 
-   (check-equal? (eval-exp (add (int 2) (int 2))) (int 4) "add simple test")
 
-   (check-exn (lambda (x) (string=? (exn-message x) "MUPL addition applied to non-number"))
-              (lambda () (eval-exp (add (int 2) (munit))))
-              "add bad argument")
+  ; Problem 1
+  (check-equal? (racketlist->mupllist [list 1 2 3]) (apair 1 (apair 2 (apair 3 munit))) "Problem1")
+ 
+  ; Problem 2
+  (check-equal? (mupllist->racketlist (apair 1 (apair 2 (apair 3 munit)))) [list 1 2 3] "Problem2")
+ 
+  ; Problem 3
+  (check-equal? (eval-exp (add (int 2) (int 2))) (int 4) "Problem3: add simple test")
+ 
+  (check-equal? (eval-exp (isgreater (int 2) (int 2))) (int 0) "Problem3: isgreater simple test1")
+ 
+  (check-equal? (eval-exp (isgreater (int 3) (int 2))) (int 1) "Problem3: isgreater simple test2")
+ 
+  (check-equal? (eval-exp (isgreater (int 2) (int -1))) (int 1) "Problem3: isgreater simple test3")
+ 
+  (check-equal? (eval-exp (isgreater (int -2) (int -1))) (int 0) "Problem3: isgreater simple test4")
+ 
+  (check-equal? (eval-exp (ifnz (int -2) (int -1) (int -2))) (int -2) "Problem3: ifnz simple test1")
+ 
+  (check-equal? (eval-exp (ifnz (int 0) (int -1) (int -2))) (int -1) "Problem3: ifnz simple test2")
+ 
+  (check-equal? (eval-exp (mlet "foo" (int 1)
+                            (add (var "foo") (int 1)))) (int 2) "Problem3: mlet simple test1")
 
-   (check-equal? (mupllist->racketlist
-                  (eval-exp (call (call mupl-all-gt (int 9))
-                                  (racketlist->mupllist 
-                                   (list (int 10) (int 9) (int 15))))))
-                 (list (int 10) (int 15))
-                 "provided combined test using problems 2, 3, and 5")
-   ))
+  (check-equal? (eval-exp (mlet "foo" (int 1)
+                           (mlet "bar" (int 2)
+                            (add (var "foo") (var "bar"))))) (int 3) "Problem3: mlet simple test2")
+ 
+  (check-equal? (eval-exp (first (apair (int 1) (int 2)))) (int 1) "Problem3: apair/first simple test")
+
+  (check-equal? (eval-exp (second (apair (int 1) (int 2)))) (int 2) "Problem3: apair/second simple test")
+
+  (check-equal? (eval-exp
+                 (call (eval-exp (fun "plus" "delta" (add (int 1) (var "delta")))) (int 2)))
+                 (int 3) "Problem3: call/fun test1")
+
+  (check-exn (lambda (x) (string=? (exn-message x) "MUPL addition applied to non-number"))
+             (lambda () (eval-exp (add (int 2) (munit))))
+             "Problem3: add bad argument")
+
+  ; (check-equal? (mupllist->racketlist
+  ;                (eval-exp (call (call mupl-all-gt (int 9))
+  ;                                (racketlist->mupllist 
+  ;                                 (list (int 10) (int 9) (int 15))))))
+  ;               (list (int 10) (int 15))
+  ;               "provided combined test using problems 2, 3, and 5")
+  ))
 
 (require rackunit/text-ui)
 ;; runs the test
